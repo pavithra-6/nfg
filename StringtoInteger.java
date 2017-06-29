2eae350db1181de2a3ff24e809b1e48ea5039769
pavithra-6 <paviikannan1996@gmail.com>
2017-06-29 10:20:00 +0530

    Add files via upload

---
+++ StringtoInteger.java
@@ -0,0 +1,12 @@
+package sumave1;
+
+public class StringtoInteger {
+
+	public static void main(String[] args) {
+		String s1="40";
+		int i=Integer.parseInt(s1);
+		System.out.println(i);
+		
+	}
+
+}
